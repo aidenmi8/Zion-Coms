8e87a59cc297b3d811c3ee9d99c1d3563f0716a4
Aiden Mi8 <aidenmi8@users.noreply.github.com>
2026-07-25 23:56:30 -0400

    fix(ios): sign Flutter native frameworks for devices

--- mobile/test/ios_native_metadata_test.dart
+++ mobile/test/ios_native_metadata_test.dart
@@ -24,4 +24,18 @@ void main() {
       contains(r'PRODUCT_BUNDLE_IDENTIFIER = "$(BUNDLE_IDENTIFIER)";'),
     );
   });
+
+  test('iOS device builds re-sign Flutter native asset frameworks', () {
+    final project = File(
+      'ios/Runner.xcodeproj/project.pbxproj',
+    ).readAsStringSync();
+
+    expect(project, contains('[Zion] Sign Flutter Native Assets'));
+    expect(project, contains('Flutter/sign_native_assets.sh'));
+
+    final script = File('ios/Flutter/sign_native_assets.sh').readAsStringSync();
+    expect(script, contains('io.flutter.flutter.native-assets.*'));
+    expect(script, contains('EXPANDED_CODE_SIGN_IDENTITY'));
+    expect(script, contains('/usr/bin/codesign --force --sign'));
+  });
 }
